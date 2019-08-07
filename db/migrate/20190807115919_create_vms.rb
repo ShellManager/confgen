f25d2bd9fb65783a7552fa1367a2f83ecc3847d2
Theo Morra <td512@td512.io>
2019-08-08 01:43:01 +1200

    Updates migration to support Ubuntu's naming

--- db/migrate/20190807115919_create_vms.rb
+++ db/migrate/20190807115919_create_vms.rb
@@ -4,7 +4,7 @@ class CreateVms < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :mac
       t.string :os
-      t.integer :version
+      t.string :version
       t.string :ip
       t.string :status
       t.timestamps
