3280994a2f962fe189c520301a172f3bc9ff438c
Keanu73 <guestone63@gmail.com>
2019-08-08 09:20:00 +0100

    Kickstart script

--- app/controllers/conf_controller.rb
+++ app/controllers/conf_controller.rb
@@ -10,4 +10,15 @@ class ConfController < ApplicationController
             render 'conf/ipxe_install' 
         end
     end
+    def kickstart
+        vm = Vm.find_by(:mac => params[:mac])
+            @os = vm.os
+            @name = vm.name
+            @version = vm.version
+            @hostname = vm.name
+            @ip = vm.ip
+            gw_oct = vm.ip.split('.')
+            @gw = "#{gw_oct[0]}.#{gw_oct[1]}.#{gw_oct[2]}.1"
+            render 'conf/kickstart_install' 
+    end
 end
